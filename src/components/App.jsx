09ad1ad203d6bec9c6d631f1b5081ce63aefb359
Vitor Ribeiro Baradel <160681696+ScatmanVit@users.noreply.github.com>
2025-05-26 19:17:00 -0300

    teste de mudança

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App" data-theme={dark ? "dark" : "light"}>
             <Toggle isChecked={dark} handleChange={() => setDark(!dark)} />
-            <h1 className="title">Hello World</h1>
+            <h1 className="title"> Hello World</h1>
             <div className="box">
                 <h2>This is a box</h2>
             </div>
