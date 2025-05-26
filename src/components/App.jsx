310c0e0130e992b25c7de9f5863deb6fafa4a7dc
Vitor Ribeiro Baradel <160681696+ScatmanVit@users.noreply.github.com>
2025-05-26 19:42:58 -0300

    Projeto finalizado e funcionando, aberto para futuras novas implementações

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,16 @@ import {Toggle} from "./Toggle.jsx"
 import React, { useState } from "react";
 
 function App() {
-    const [dark, setdark] = useState(false);
+    const [dark, setDark] = useState(false);
 
     return (
         <div className="App" data-theme={dark ? "dark" : "light"}>
-            <Toggle isChecked={dark} handleChange={() => setDark(!dark)} />
+            <Toggle isChecked={dark}
+                handleChange={() => setDark(!dark)}
+            />
             <h1 className="title"> Hello World</h1>
             <div className="box">
-                <h2>This is a box</h2>
+                <h2>This is a  box</h2>
             </div>
         </div>
     );
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -5,10 +5,11 @@ export const Toggle = ({ handleChange, isChecked }) => {
     return (
         <div className="toggle-container">
         <input
-            type="text"
+            type="checkbox"
             id="check"
             className="toggle"
             onChange={handleChange}
+            checked={isChecked}
         />
         <label htmlFor="check">Dark Mode</label>
         </div>
